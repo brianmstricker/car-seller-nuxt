f7d633b17a8d6734950ef7db56493d7616c85968
brianmstricker <brianmstricker@gmail.com>
2023-12-15 16:40:29 -0500

    update: why buy from us image sizes

--- components/IndexWhyBuySellSection.vue
+++ components/IndexWhyBuySellSection.vue
@@ -7,15 +7,15 @@
    <div>
     <NuxtImg
      src="images/trusted.png"
-     class="rounded-full w-[150px] h-[150px] md:w-[200px] md:h-[200px] object-cover ring ring-black/10 mx-auto"
-     sizes="150px md:200px"
-     width="200px"
-     height="200px"
+     class="rounded-full w-[150px] h-[150px] md:w-[175px] md:h-[175px] object-cover ring ring-black/20 mx-auto"
+     sizes="150px md:175px"
+     width="175px"
+     height="175px"
     />
     <div class="flex items-center justify-center mt-2 relative">
      <UIcon class="relative -left-2" name="i-heroicons-check-20-solid" />
      <span class="md:text-lg"
-      >Trusted<br class="hidden min-[500px]:block" />
+      >Trusted<br class="hidden min-[500px]:block lg:hidden" />
       Sellers</span
      >
     </div>
@@ -23,15 +23,15 @@
    <div>
     <NuxtImg
      src="images/quality.jpg"
-     class="rounded-full w-[150px] h-[150px] md:w-[200px] md:h-[200px] object-cover ring ring-black/10 mx-auto"
-     sizes="150px md:200px"
-     width="200px"
-     height="200px"
+     class="rounded-full w-[150px] h-[150px] md:w-[175px] md:h-[175px] object-cover ring ring-black/20 mx-auto"
+     sizes="150px md:175px"
+     width="175px"
+     height="175px"
     />
     <div class="flex items-center justify-center mt-2 relative">
      <UIcon class="relative -left-2" name="i-heroicons-hand-thumb-up" />
      <span class="md:text-lg"
-      >Quality<br class="hidden min-[500px]:block" />
+      >Quality<br class="hidden min-[500px]:block lg:hidden" />
       Vehicles</span
      >
     </div>
@@ -39,15 +39,15 @@
    <div>
     <NuxtImg
      src="images/warranty.png"
-     class="rounded-full w-[150px] h-[150px] md:w-[200px] md:h-[200px] object-cover ring ring-black/10 mx-auto"
-     sizes="150px md:200px"
-     width="200px"
-     height="200px"
+     class="rounded-full w-[150px] h-[150px] md:w-[175px] md:h-[175px] object-cover ring ring-black/20 mx-auto"
+     sizes="150px md:175px"
+     width="175px"
+     height="175px"
     />
     <div class="flex items-center justify-center mt-2 relative">
      <UIcon class="relative -left-2" name="i-heroicons-document-text" />
      <span class="md:text-lg"
-      >Warranty<br class="hidden min-[500px]:block" />
+      >Warranty<br class="hidden min-[500px]:block lg:hidden" />
       Coverage</span
      >
     </div>
@@ -64,6 +64,7 @@
   </div>
   <div>
    <h2 class="text-3xl font-semibold italic">sell your vehicle with ease</h2>
+   <!-- todo: this -->
   </div>
  </div>
 </template>
