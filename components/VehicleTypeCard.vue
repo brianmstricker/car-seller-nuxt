376683acf034cb43a9097c8db1ce633bc647907c
brianmstricker <brianmstricker@gmail.com>
2023-12-14 23:34:20 -0500

    update: image optimization

--- components/VehicleTypeCard.vue
+++ components/VehicleTypeCard.vue
@@ -2,7 +2,13 @@
  <div
   class="bg-white rounded-md shadow-[0px_4px_14px_0px_rgba(0,0,0,0.10)] hover:shadow-[0px_4px_14px_0px_rgba(0,0,0,0.35)] p-4 border border-gray-300/50 cursor-pointer transition-all duration-300"
  >
-  <NuxtImg :src="image" class="object-contain mx-auto w-48 h-20" />
+  <NuxtImg
+   :src="image"
+   class="object-contain mx-auto w-48 h-20"
+   sizes="200px"
+   width="192px"
+   height="80px"
+  />
   <h3 class="text-center text-xl font-bold">{{ name }}</h3>
  </div>
 </template>
